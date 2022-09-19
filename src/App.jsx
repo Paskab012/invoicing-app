ebb7dbe2353e8abc90320d869a02d129952d89fa
Paskab012 <pascalkabikampan@gmail.com>
2022-09-19 21:39:29 +0200

    Add tailwindcss configuration for better UI UX

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import { Button } from 'semantic-ui-react';
 
 function App() {
@@ -6,6 +5,7 @@ function App() {
     <div className='App'>
       <h1>Hello, here is my app</h1>
       <Button>Click Here</Button>
+      <h1 class='text-2xl font-bold underline text-red-400'>Hello world!</h1>
     </div>
   );
 }
