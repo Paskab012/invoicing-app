ea8c176d7437e584997879a6c40f7f380ff7d949
Paskab012 <pascalkabikampan@gmail.com>
2022-09-25 21:24:52 +0200

    Implement the confirm page

--- src/components/ConfirmPassword/index.jsx
+++ src/components/ConfirmPassword/index.jsx
@@ -5,8 +5,8 @@ function ConfirmPassword() {
   return (
     <>
       <section className='h-screen'>
-        <div className='px-4 lg:px-16 h-full text-gray-800 bg-white flex flex-col w-full justify-center items-center content-center'>
-          <div className=' w-full flex items-start'>
+        <div className='flex flex-col items-center content-center justify-center w-full h-full px-4 text-gray-800 bg-white lg:px-16'>
+          <div className='flex items-start w-full '>
             <img
               src='/src/assets/exuus.webp'
               className='w-[60px] h-full object-cover object-center'
@@ -15,42 +15,42 @@ function ConfirmPassword() {
           </div>
 
           <div className='flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-[80%] g-2 w-full'>
-            <div className='grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0'>
+            <div className='mb-12 grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 md:mb-0'>
               <img
                 src='/src/assets/invoice.svg'
-                className='w-full hidden xl:block'
+                className='hidden w-full xl:block'
                 alt='logo'
               />
             </div>
             <div className='xl:ml-20 lg:w-[35%] h-fit md:w-8/12 mb-12 md:mb-0 bg-[#f2f2f2] rounded-lg p-6 shadow-sm'>
               <form>
-                <p className='text-3xl font-bold text-center mb-8 text-titleColor'>
-                  Confirm Password
+                <p className='mb-8 text-3xl font-bold text-center text-titleColor'>
+                  Confirm Code
                 </p>
 
                 <div className='mb-6'>
                   <input
                     type='password'
-                    className='form-control block w-full px-3 py-3 text-sm font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
+                    className='block w-full px-3 py-3 m-0 text-sm font-normal text-gray-700 transition ease-in-out bg-white border border-gray-300 border-solid rounded form-control bg-clip-padding focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
                     id='exampleFormControlInput2'
                     placeholder='Input code'
                   />
                 </div>
 
-                <div className='flex justify-between items-center mb-6'>
+                <div className='flex items-center justify-between mb-6'>
                   <a href='#!' className='text-gray-800'>
                     This code will expire in 3mins!
                   </a>
                 </div>
 
-                <div className='text-center lg:text-left  flex items-center justify-start'>
+                <div className='flex items-center justify-start text-center lg:text-left'>
                   <Link
-                    to='/login'
-                    className='text-titleColor hover:text-red-700 focus:text-red-700  transition duration-200 ease-in-out mr-4'
+                    to='/'
+                    className='mr-4 transition duration-200 ease-in-out text-titleColor hover:text-red-700 focus:text-red-700'
                   >
                     <button
                       type='button'
-                      className='inline-block px-7 bg-brightRed py-2  border border-brightRed text-white hover:text-white  text-sm leading-snug uppercase rounded shadow-md  hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0 font-bold active:shadow-lg transition duration-150 ease-in-out'
+                      className='inline-block py-2 text-sm font-bold leading-snug text-white uppercase transition duration-150 ease-in-out border rounded shadow-md px-7 bg-brightRed border-brightRed hover:text-white hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg'
                     >
                       <svg
                         xmlns='http://www.w3.org/2000/svg'
@@ -70,9 +70,9 @@ function ConfirmPassword() {
                   </Link>
                   <button
                     type='button'
-                    className='inline-block px-7 py-3 hover:bg-brightRed border border-brightRed text-brightRed hover:text-white  text-sm leading-snug uppercase rounded shadow-md  hover:shadow-lg  focus:shadow-lg focus:outline-none focus:ring-0 font-bold active:shadow-lg transition duration-150 ease-in-out'
+                    className='inline-block py-3 text-sm font-bold leading-snug uppercase transition duration-150 ease-in-out border rounded shadow-md px-7 hover:bg-brightRed border-brightRed text-brightRed hover:text-white hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg'
                   >
-                    Confirm Password
+                    Confirm Code
                   </button>
                 </div>
               </form>
